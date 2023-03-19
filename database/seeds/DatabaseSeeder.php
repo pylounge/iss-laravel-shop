28eca17484df177db972949a0e045620db9c9bfe
Макс <78209508+pylounge@users.noreply.github.com>
2023-03-19 11:45:46 +0300

    Add files via upload

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeder;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         $this->call(UsersTableSeeder::class);
+         $this->call(CategoriesTableSeeder::class);
+         $this->call(ProductsTableSeeder::class);
+         $this->call(DatabaseSeeder::class);
+    }
+}
